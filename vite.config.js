bdb44168f3bca074bc4b677c771c14ce75c18032
Richard Powell <richard.powell@shopify.com>
2025-06-09 17:12:58 -0400

    Future flag: Adopt v3_lazyRouteDiscovery, v3_throwAbortReason, v3_relativeSplatPath and v3_fetcherPersist

--- vite.config.js
+++ vite.config.js
@@ -46,6 +46,10 @@ export default defineConfig({
       future: {
         v3_singleFetch: true,
         v3_routeConfig: true,
+        v3_lazyRouteDiscovery: true,
+        v3_throwAbortReason: true,
+        v3_relativeSplatPath: true,
+        v3_fetcherPersist: true,
       },
     }),
     tsconfigPaths(),
