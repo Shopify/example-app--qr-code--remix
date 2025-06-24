7e2ee0d6cd0d1478aadc2e47fafac417d97dec5d
Richard Powell <richard.powell@shopify.com>
2025-06-24 09:22:09 -0400

    Shopify App: Update AppProvider, since the AppProvider from @shopify/shopify-app-react-router is simpler

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,29 +1,26 @@
 import { Outlet, useLoaderData, useRouteError } from "react-router";
-import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 
 import { authenticate } from "../shopify.server";
 
-export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
-
-export async function loader({ request }) {
+export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
-  return { apiKey: process.env.SHOPIFY_API_KEY };
-}
+  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+};
 
 export default function App() {
   const { apiKey } = useLoaderData();
 
   return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
+    <AppProvider embedded apiKey={apiKey}>
       <Outlet />
     </AppProvider>
   );
 }
 
-// Shopify needs React Router to catch some thrown responses, so that their headers are included in the response.
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
   return boundary.error(useRouteError());
 }
